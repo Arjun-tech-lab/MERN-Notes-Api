69fc3ea56fa6f2cd6a092561c498a1c1ee3a21cd
Arjun Indavara <arjunindavara@Arjuns-MacBook-Air.local>
2025-09-17 23:14:36 +0530

    final commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,47 +1,19 @@
 import express from "express";
-import dotenv from "dotenv";
-import connectDB from "./config/db.js";
-import notesRoutes from "./routes/notesRoutes.js";
-import rateLimiter from "./middlewear/ratelimiter.js";
 import cors from "cors";
+import dotenv from "dotenv";
 import path from "path";
-import { fileURLToPath } from "url";
-import helmet from "helmet";
+
+import notesRoutes from "./routes/notesRoutes.js";
+import  connectDB from "./config/db.js";
+import rateLimiter from "./middlewear/ratelimiter.js";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-
-const cspDirectives = {
-  defaultSrc: ["'self'"],
-  scriptSrc: ["'self'"],
-  styleSrc: ["'self'", "https:"],
-  imgSrc: ["'self'", "data:", "https:"],
-  connectSrc: ["'self'"], // base rule
-};
-
-if (process.env.NODE_ENV !== "production") {
- 
-  cspDirectives.connectSrc.push("http://localhost:5173");
-} else {
-
-  cspDirectives.connectSrc.push("https://your-production-frontend.com");
-}
-
-app.use(
-  helmet({
-    contentSecurityPolicy: {
-      directives: cspDirectives,
-    },
-  })
-);
-
-// Middleware to parse JSON request bodies
+// middleware
 if (process.env.NODE_ENV !== "production") {
   app.use(
     cors({
@@ -49,23 +21,22 @@ if (process.env.NODE_ENV !== "production") {
     })
   );
 }
-
-app.use(express.json());
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
 app.use(rateLimiter);
 
-// Mount routes
+
 app.use("/api/notes", notesRoutes);
 
-// Serve frontend in production
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
 connectDB().then(() => {
-  app.listen(5001, () => {
-    console.log("server started on port :5001");
+  app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
   });
 });
